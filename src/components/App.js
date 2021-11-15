fcd39894e9999ef49dc06f8fe38f20a1bec0f20e
Roman PHILIP <romanphlp@gmail.com>
2021-11-15 23:14:16 +0100

    End of second part Tuto React

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import Banner from './Banner'
 import Cart from './Cart'
+import Footer from './Footer';
 import ShoppingList from './ShoppingList';
 
 function App() {
   return <div><Banner />
               <Cart />
               <ShoppingList />
+              <Footer />
           </div>
 }
 
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,34 @@
-const plantList = [
-    'monstera',
-    'ficus lyrata',
-    'pothos argenté',
-    'yucca',
-    'palmier'
-]
+import { plantList } from '../datas/plantList'
+import PlantItem from './PlantItem'
+import '../style/ShoppingList.css'
 
 function ShoppingList() {
-    return (
-        <ul>
-            {plantList.map((plant, index) => (
-                <li key={`${plant}-${index}`}>{ plant }</li>
-            ))}
-        </ul>
-    )
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
+
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul className='lmj-plant-list'>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						id={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+                        />
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default ShoppingList
