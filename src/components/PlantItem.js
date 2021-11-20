bf9b819973db81edee74566706427a80a5b308b2
Roman PHILIP <romanphlp@gmail.com>
2021-11-20 18:29:13 +0100

    Last commit: end of react tuto

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,15 +1,18 @@
 import CareScale from './CareScale'
 import '../style/PlantItem.css'
 
+function handleClick(plantName) {
+	alert(`Vous voulez acheter 1 ${plantName}? Très bon choix 🌱✨`)
+}
 
-
-function PlantItem({ id, cover, name, water, light }) {
+function PlantItem({ cover, name, water, light, price }) {
 	return (
-		<li key={id} className='lmj-plant-item'>
+		<li className='lmj-plant-item' onClick={() => handleClick}>
+			<span className='lmj-plant-item-price'>{price}€</span>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
-				<CareScale careType='water' scaleValue={water}/>
+				<CareScale careType='water' scaleValue={water} />
 				<CareScale careType='light' scaleValue={light} />
 			</div>
 		</li>
